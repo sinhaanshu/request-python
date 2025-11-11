9480ac0c8c6a4709e1b1018d770a50a18f50d5a6
Anshu Sinha <anshusinha4@gmail.com>
2025-11-11 13:49:58 +0530

    Update test.py
    add dummy keys

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import requests
 # Replace with your desired endpoint
 url = 'https://httpbin.org/get'
 print(url)
+access_key=12345
+secret_access_key=6789
 
 # Send GET request
 print("Sending Requests")
